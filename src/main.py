81ad08821c4c3163523303b3225379db8c41dbec
Semyon Sazanchuk <siamions@compatibl.com>
2024-03-22 23:05:42 +0300

    Implement routes.

--- src/main.py
+++ src/main.py
@@ -1,31 +1,44 @@
+import os
+
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 from pydantic import BaseModel
+import redis
 
 
 class ContactData(BaseModel):
     address: str
     phone: int
 
+
 app = FastAPI()
+redis_host = os.environ.get('REDIS_HOST', 'localhost')
+redis_port = int(os.environ.get('REDIS_PORT', 6379))
+redis_client = redis.Redis(host=redis_host, port=redis_port, decode_responses=True)
 
-@app.post('/write_date')
-async def write_data(record: ContactData):
+
+@app.post('/write_data')
+async def write_data(response: Response, record: ContactData):
     """
-    Save phone number by address.
+    Save phone number and address to storage.
     """
+    result = redis_client.set(record.phone, record.address)
+    if not result:
+        response.status_code = 500
 
-    pass
 
-
-@app.post('/write_date')
-async def check_data():
+@app.get('/check_data')
+async def check_data(response: Response, phone: int):
     """
-    Get address by phone number.
+    Get address by phone number from storage.
     """
-
-    pass
+    result = redis_client.get(phone)
+    if result is None:
+        response.status_code = 400
+    return result
 
 
 if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=8008)
+    host = os.environ.get('APP_HOST', '0.0.0.0')
+    port = int(os.environ.get('APP_PORT', 8008))
+    uvicorn.run(app, host=host, port=port)
